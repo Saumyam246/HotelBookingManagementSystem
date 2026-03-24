20f02aedf8ef7804f323023c825865f8d0a84704
Saumyam246 <saumyam246@gmail.com>
2026-03-24 14:48:17 +0530

    COMMIT MESSAGE

---
+++ App/src/UseCase4RoomSearch.java
@@ -0,0 +1,28 @@
+public class UseCase4RoomSearch {
+
+    public static void main(String[] args) {
+
+        System.out.println("Room Search");
+        System.out.println();
+
+        System.out.println("Single Room:");
+        System.out.println("Beds: 1");
+        System.out.println("Size: 250 sqft");
+        System.out.println("Price per night: 1500.0");
+        System.out.println("Available: 5");
+        System.out.println();
+
+        System.out.println("Double Room:");
+        System.out.println("Beds: 2");
+        System.out.println("Size: 400 sqft");
+        System.out.println("Price per night: 2500.0");
+        System.out.println("Available: 3");
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        System.out.println("Beds: 3");
+        System.out.println("Size: 750 sqft");
+        System.out.println("Price per night: 5000.0");
+        System.out.println("Available: 2");
+    }
+}
